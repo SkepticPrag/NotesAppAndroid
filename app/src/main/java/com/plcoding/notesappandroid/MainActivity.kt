ad6ce3265c7d4782eb3fbb112e61c940eba75675
Germán Lobbia <germanlobbia@gmail.com>
2023-05-03 16:55:40 -0300

    [U] Updated Implementation Versions

--- app/src/main/java/com/plcoding/notesappandroid/MainActivity.kt
+++ app/src/main/java/com/plcoding/notesappandroid/MainActivity.kt
@@ -3,18 +3,13 @@ package com.plcoding.notesappandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.plcoding.notesappandroid.ui.theme.CleanArchitectureNoteAppTheme
+import com.plcoding.notesappandroid.ui.theme.NotesAppAndroid
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CleanArchitectureNoteAppTheme {
+            NotesAppAndroid {
                 
             }
         }
