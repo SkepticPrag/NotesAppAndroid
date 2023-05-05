8c012d862f93b4dece3bbc24e3c8b165ed1fdbd0
Germán Lobbia <germanlobbia@gmail.com>
2023-05-04 22:52:40 -0300

    [W] Working on NotesViewModel

---
+++ app/src/main/java/com/notesappandroid/feature_note/presentation/notes/components/NotesState.kt
@@ -0,0 +1,13 @@
+package com.notesappandroid.feature_note.presentation.notes.components
+
+import com.notesappandroid.feature_note.domain.model.Note
+import com.notesappandroid.feature_note.domain.util.NoteOrder
+import com.notesappandroid.feature_note.domain.util.OrderType
+
+data class NotesState(
+
+    val notes: List<Note> = emptyList(),
+    val noteOrder: NoteOrder= NoteOrder.Date(OrderType.Descending),
+    val isOrderSectionVisible: Boolean = false
+
+)
