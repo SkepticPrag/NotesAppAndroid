84efd609c1728a9f811e63e50d07b84a00854f5b
Germán Lobbia <germanlobbia@gmail.com>
2023-05-05 23:57:35 -0300

    [U] Updated ViewModel

--- app/src/main/java/com/notesappandroid/feature_note/presentation/add_edit_note/components/AddEditNoteViewModel.kt
+++ app/src/main/java/com/notesappandroid/feature_note/presentation/add_edit_note/components/AddEditNoteViewModel.kt
@@ -4,11 +4,14 @@ import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.graphics.toArgb
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.notesappandroid.feature_note.domain.model.InvalidNoteException
 import com.notesappandroid.feature_note.domain.model.Note
 import com.notesappandroid.feature_note.domain.use_case.NoteUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -28,8 +31,54 @@ class AddEditNoteViewModel @Inject constructor(
     private val _eventFlow = MutableSharedFlow<UiEvent>()
     val eventFlow = _eventFlow.asSharedFlow()
 
+    private var currentNoteId:Int? = null
+
+    fun onEvent(event: AddEditNoteEvent)
+    {
+        when(event)
+        {
+            is AddEditNoteEvent.EnteredTitle->{
+                _noteTitle.value = noteTitle.value.copy(text = event.value)
+            }
+            is AddEditNoteEvent.ChangeTitleFocus ->
+            {
+                _noteTitle.value= noteTitle.value.copy(isHintVisible = !event.focusState.isFocused && noteTitle.value.text.isBlank())
+            }
+            is AddEditNoteEvent.EnteredContent->{
+                _noteContent.value = noteContent.value.copy(text = event.value)
+            }
+            is AddEditNoteEvent.ChangeContentFocus ->
+            {
+                _noteContent.value= noteContent.value.copy(isHintVisible = !event.focusState.isFocused && _noteContent.value.text.isBlank())
+            }
+            is AddEditNoteEvent.ChangeColor ->{
+                _noteColor.value = event.color
+            }
+            is AddEditNoteEvent.SaveNote -> {
+                viewModelScope.launch {
+                    try
+                    {
+                        noteUseCases.addNote(
+                            Note(
+                                title = noteTitle.value.text,
+                                content = noteContent.value.text,
+                                timeStamp = System.currentTimeMillis(),
+                                color = noteColor.value,
+                                id = currentNoteId
+                            )
+                        )
+                    } catch (e: InvalidNoteException) {
+
+                    }
+                }
+            }
+        }
+    }
+
     sealed class UiEvent{
         data class ShowSnackBar(val message: String): UiEvent()
         object SaveNote: UiEvent()
     }
+
+
 }
